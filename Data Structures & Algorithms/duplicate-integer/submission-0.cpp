161418cd19482211442adeb934af34936c93c2fe
Andrew <104359161+Andrew7441@users.noreply.github.com>
2026-05-08 14:56:41 +0300

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        if(st.size() == nums.size()) return false;
+
+        return true;
+        
+    }
+};
---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+
+        for(int i : nums){
+            if(st.count(i)){
+                return true;
+            }else{
+                st.insert(i);
+            }
+        }
+        return false;
+    }
+};
