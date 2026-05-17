dc987291df0c6d0f827bf643a80a589aa0ae3496
Andrew <104359161+Andrew7441@users.noreply.github.com>
2026-05-17 18:58:20 +0300

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& board, int i, int j){
+        int m = board.size(), n = board[0].size();
+
+        if(i < 0 || i >= m || j < 0 || j >= n) return;
+        if(board[i][j] != 'O') return;
+
+        board[i][j] = 'S';
+
+        dfs(board, i, j + 1);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i - 1, j);
+    }
+    void solve(vector<vector<char>>& board) {
+        int m = board.size(), n = board[0].size();
+
+        for(int c = 0; c < n; c++){
+            if(board[0][c] == 'O') dfs(board, 0, c);
+            if(board[m-1][c] == 'O') dfs(board, m-1,c);
+        }
+
+        for(int r = 0; r < m; r++){
+            if(board[r][0] == 'O') dfs(board, r, 0);
+            if(board[r][n-1] == 'O') dfs(board, r, n - 1);
+        }
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == 'O') board[i][j] = 'X';
+                else if(board[i][j] == 'S') board[i][j] = 'O';
+            }
+        }
+    }
+};
